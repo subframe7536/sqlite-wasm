9eaf4e678355e630f2ab30e5b81e045ba7b12919
subframe7536 <1667077010@qq.com>
2023-11-14 13:01:26 +0800

    fix: support check

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -1,13 +1,17 @@
 import { useIdbStorage } from '@subframe7536/sqlite-wasm/idb'
-import { initSQLite, isModuleWorkerSupport, isOpfsSupported } from '@subframe7536/sqlite-wasm'
-import Worker from './worker?worker'
+import { initSQLite, isIdbSupported, isModuleWorkerSupport, isOpfsSupported } from '@subframe7536/sqlite-wasm'
+import OpfsWorker from './worker?worker'
 
 initSQLite(useIdbStorage('test', {
   // url: 'https://cdn.jsdelivr.net/gh/rhashimoto/wa-sqlite@v0.9.9/dist/wa-sqlite-async.wasm',
 }))
   .then(async ({ run }) => {
-    console.log(await isOpfsSupported())
-    console.log(await isModuleWorkerSupport())
+    const supportModuleWorker = isModuleWorkerSupport()
+    const supportIDB = isIdbSupported()
+    const supportOPFS = await isOpfsSupported()
+    console.log('support module worker:', supportModuleWorker)
+    console.log('support IDBBatchAtomicVFS:', supportIDB)
+    console.log('support AccessHandlePoolVFS:', supportOPFS)
     console.log(await run('CREATE TABLE t1(a INTEGER, b INTEGER, c VARCHAR(100));'))
     console.log(await run('INSERT INTO t1 VALUES(1, 19147, \'nineteen thousand one hundred forty-seven\');'))
     console.log(await run('INSERT INTO t1 VALUES(2, 26008, \'twenty-six thousand eight\');'))
@@ -15,5 +19,5 @@ initSQLite(useIdbStorage('test', {
     console.log(await run('select * from t1'))
   })
 
-const worker = new Worker()
+const worker = new OpfsWorker()
 worker.postMessage('')
--- src/utils.ts
+++ src/utils.ts
@@ -10,63 +10,68 @@ export function isIdbSupported() {
  * check if [OPFS SyncAccessHandle](https://developer.mozilla.org/en-US/docs/Web/API/FileSystemSyncAccessHandle) supported
  */
 export function isOpfsSupported(): Promise<boolean> {
+  // must call and test, see https://stackoverflow.com/questions/76113945/file-system-access-api-on-safari-ios-createsyncaccesshandle-unknownerror-i
   const inner = async () => {
     const root = await navigator?.storage.getDirectory?.()
     if (!root) {
       return false
     }
-    const checkFileName = '_CHECK'
     try {
-      const handle = await root.getFileHandle(checkFileName, { create: true })
-      return 'createSyncAccessHandle' in handle
+      const handle = await root.getFileHandle('_CHECK', { create: true })
+      // @ts-expect-error check
+      const access = await handle.createSyncAccessHandle()
+      access.close()
+      return true
     } catch {
       return false
     } finally {
-      await root.removeEntry(checkFileName)
+      await root.removeEntry('_CHECK')
     }
   }
-  const isInWorker = globalThis && 'importScripts' in globalThis
-  return isInWorker
+  return 'importScripts' in globalThis
     ? inner()
-    : runWithWorker(`(${inner})().then(postMessage)`)
+    : new Promise<boolean>((resolve, reject) => {
+      if (typeof Worker === 'undefined') {
+        resolve(false)
+      }
+
+      const url = URL.createObjectURL(
+        new Blob(
+          [`onmessage=()=>{(${inner})().then(postMessage)}`],
+          { type: 'text/javascript' },
+        ),
+      )
+
+      const worker = new Worker(url, { type: 'module' })
+      worker.onmessage = ({ data }) => {
+        URL.revokeObjectURL(url)
+        worker.terminate()
+        resolve(data)
+      }
+      worker.onerror = () => {
+        worker.terminate()
+        reject(false)
+      }
+      worker.postMessage('')
+    }).catch(() => false)
 }
 
 /**
- * check `new Worker(url, { type: 'module' }) support`
+ * check `new Worker(url, { type: 'module' })` support
+ *
+ * {@link https://stackoverflow.com/questions/62954570/javascript-feature-detect-module-support-for-web-workers Reference}
  */
-export function isModuleWorkerSupport(): Promise<boolean> {
-  return runWithWorker('postMessage(true)')
-}
-
-/**
- * check `new Worker(url, { type: 'module' }) support`
- */
-function runWithWorker(code: string): Promise<boolean> {
-  return new Promise<boolean>((resolve, reject) => {
-    if (typeof Worker === 'undefined') {
-      resolve(false)
-    }
-
-    const url = URL.createObjectURL(
-      new Blob(
-        [`onmessage=()=>{${code}}`],
-        { type: 'text/javascript' },
-      ),
-    )
-
-    const worker = new Worker(url, { type: 'module' })
-
-    worker.onmessage = ({ data }) => {
-      URL.revokeObjectURL(url)
-      worker.terminate()
-      resolve(data)
-    }
-
-    worker.onerror = () => {
-      worker.terminate()
-      reject(false)
-    }
-
-    worker.postMessage('')
-  }).catch(() => false)
+export function isModuleWorkerSupport(): boolean {
+  let supports = false
+  try {
+    new Worker('data:,', {
+      // @ts-expect-error check assign
+      get type() {
+        supports = true
+      },
+    }).terminate()
+  } finally {
+    // eslint-disable-next-line no-unsafe-finally
+    return supports
+  }
 }
