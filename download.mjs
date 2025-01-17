f9737c78efa46ff573cf4c0efedb6f4cc92534d1
subframe7536 <1667077010@qq.com>
2025-01-17 09:19:50 +0800

    chore: upgrade wasm

--- download.mjs
+++ download.mjs
@@ -20,9 +20,7 @@ async function getLatestTag() {
     }
 
     // The tags are returned in chronological order, so the first one is the latest
-    const latestTag = tags[0].name
-    console.log(JSON.stringify(tags[0]))
-    return latestTag
+    return tags[0].name
   } catch (error) {
     console.error('Error fetching latest tag:', error)
     return null
@@ -40,42 +38,38 @@ async function downloadAndExtractRelease(tag, outputDir) {
   const releaseUrl = `https://github.com/${REPO}/releases/${tag}`
   const downloadUrl = `https://${githubProxy}/${REPO}/releases/download/${tag}/wa-sqlite.dist.tgz`
   const target = `wasqlite-fts5-${tag}.tgz`
-  try {
-    if (!existsSync(target)) {
-      console.log(`Downloading from ${downloadUrl}`)
-      const resp = await fetch(downloadUrl, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/gzip',
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-        },
-      })
 
-      if (!resp.ok) {
-        throw new Error(`Download fail: ${resp.statusText}`)
-      }
-
-      writeFileSync(target, new Uint8Array(await resp.arrayBuffer()))
-    }
-
-    console.log('Extracting...')
-    await x({
-      file: target,
-      cwd: outputDir,
-    })
-
-    console.log('Updating README.md')
-
-    writeFileSync(
-      `${outputDir}/README.md`,
-      `# wa-sqlite fts5\n\nDownload from https://github.com/${REPO}\n\nTag [\`${tag}\`](${releaseUrl})\n`,
-      'utf-8',
-    )
-  } finally {
-    if (existsSync(target)) {
-      rmSync(target)
-    }
+  if (existsSync(target)) {
+    rmSync(target)
   }
+  console.log(`Downloading from ${downloadUrl}`)
+  const resp = await fetch(downloadUrl, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/gzip',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    },
+  })
+
+  if (!resp.ok) {
+    throw new Error(`Download fail: ${resp.statusText}`)
+  }
+
+  writeFileSync(target, new Uint8Array(await resp.arrayBuffer()))
+
+  console.log('Extracting...')
+  await x({
+    file: target,
+    cwd: outputDir,
+  })
+
+  console.log('Updating README.md')
+
+  writeFileSync(
+    `${outputDir}/README.md`,
+    `# wa-sqlite fts5\n\nDownload from https://github.com/${REPO}\n\nTag [\`${tag}\`](${releaseUrl})\n`,
+    'utf-8',
+  )
 }
 
 getLatestTag()
