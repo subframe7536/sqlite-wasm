7433b154857fda1285221cc5913dfdf2a0d0b7c8
subframe7536 <1667077010@qq.com>
2025-01-17 08:35:14 +0800

    chore: restructure

--- src/core.ts
+++ src/core.ts
@@ -4,9 +4,9 @@ import {
   SQLITE_OPEN_CREATE,
   SQLITE_OPEN_READONLY,
   SQLITE_OPEN_READWRITE,
-  SQLITE_ROW,
 } from 'wa-sqlite'
 import { exportDatabase, importDatabase } from './io'
+import { changes, close, lastInsertRowId, run, stream } from './utils'
 
 /**
  * Load SQLite database, presets:
@@ -59,46 +59,4 @@ export async function initSQLiteCore(
     sqliteModule,
     vfs,
   }
-}
-
-export async function close(core: SQLiteDBCore): Promise<void> {
-  await core.sqlite.close(core.pointer)
-  await core.vfs.close()
-}
-
-export function changes(core: SQLiteDBCore): number | bigint {
-  return core.sqliteModule._sqlite3_changes(core.pointer)
-}
-
-export function lastInsertRowId(core: SQLiteDBCore): number | bigint {
-  return core.sqliteModule._sqlite3_last_insert_rowid(core.pointer)
-}
-
-export async function stream(
-  core: SQLiteDBCore,
-  onData: (data: Record<string, SQLiteCompatibleType>) => void,
-  sql: string,
-  parameters?: SQLiteCompatibleType[],
-): Promise<void> {
-  const { sqlite, pointer } = core
-  for await (const stmt of sqlite.statements(pointer, sql)) {
-    if (parameters?.length) {
-      sqlite.bind_collection(stmt, parameters)
-    }
-    const cols = sqlite.column_names(stmt)
-    while (await sqlite.step(stmt) === SQLITE_ROW) {
-      const row = sqlite.row(stmt)
-      onData(Object.fromEntries(cols.map((key, i) => [key, row[i]])))
-    }
-  }
-}
-
-export async function run(
-  core: SQLiteDBCore,
-  sql: string,
-  parameters?: SQLiteCompatibleType[],
-): Promise<Array<Record<string, SQLiteCompatibleType>>> {
-  const results: any[] = []
-  await stream(core, data => results.push(data), sql, parameters)
-  return results
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 import type { BaseStorageOptions, SQLiteDBCore } from './types'
 import { SQLITE_DETERMINISTIC, SQLITE_DIRECTONLY, SQLITE_UTF8 } from 'wa-sqlite'
+import { SQLITE_ROW } from 'wa-sqlite/src/sqlite-constants.js'
 import { importDatabase } from './io'
 
 /**
@@ -176,4 +177,46 @@ export function withExistDB<T extends BaseStorageOptions>(
     ...options,
     beforeOpen: (vfs, path) => importDatabase(vfs, path, data),
   } as T
+}
+
+export async function close(core: SQLiteDBCore): Promise<void> {
+  await core.sqlite.close(core.pointer)
+  await core.vfs.close()
+}
+
+export function changes(core: SQLiteDBCore): number | bigint {
+  return core.sqliteModule._sqlite3_changes(core.pointer)
+}
+
+export function lastInsertRowId(core: SQLiteDBCore): number | bigint {
+  return core.sqliteModule._sqlite3_last_insert_rowid(core.pointer)
+}
+
+export async function stream(
+  core: SQLiteDBCore,
+  onData: (data: Record<string, SQLiteCompatibleType>) => void,
+  sql: string,
+  parameters?: SQLiteCompatibleType[],
+): Promise<void> {
+  const { sqlite, pointer } = core
+  for await (const stmt of sqlite.statements(pointer, sql)) {
+    if (parameters?.length) {
+      sqlite.bind_collection(stmt, parameters)
+    }
+    const cols = sqlite.column_names(stmt)
+    while (await sqlite.step(stmt) === SQLITE_ROW) {
+      const row = sqlite.row(stmt)
+      onData(Object.fromEntries(cols.map((key, i) => [key, row[i]])))
+    }
+  }
+}
+
+export async function run(
+  core: SQLiteDBCore,
+  sql: string,
+  parameters?: SQLiteCompatibleType[],
+): Promise<Array<Record<string, SQLiteCompatibleType>>> {
+  const results: any[] = []
+  await stream(core, data => results.push(data), sql, parameters)
+  return results
 }
