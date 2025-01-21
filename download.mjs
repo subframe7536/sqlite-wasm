676c032cebf967d13aa6dd77c7fe79f18dc0ef1b
subframe7536 <1667077010@qq.com>
2025-01-21 17:02:36 +0800

    chore: dont remove downloaded tgz

--- download.mjs
+++ download.mjs
@@ -5,8 +5,14 @@ import { x } from 'tar'
 // Read GitHub proxy from environment variable
 const githubProxy = process.env.GITHUB || 'github.com'
 const REPO = 'subframe7536/sqwab'
-
+const cliTag = process.argv[2]
 async function getLatestTag() {
+  if (cliTag) {
+    if (!cliTag.startsWith('v')) {
+      return `v${cliTag}`
+    }
+    return cliTag
+  }
   const apiURL = `https://api.github.com/repos/${REPO}/tags`
   try {
     const response = await fetch(apiURL)
@@ -40,42 +46,39 @@ async function downloadAndExtractRelease(tag, outputDir) {
   const releaseUrl = `https://github.com/${REPO}/releases/${tag}`
   const downloadUrl = `https://${githubProxy}/${REPO}/releases/download/${tag}/wa-sqlite.dist.tgz`
   const target = `wasqlite-fts5-${tag}.tgz`
-  try {
-    if (!existsSync(target)) {
-      console.log(`Downloading from ${downloadUrl}`)
-      const resp = await fetch(downloadUrl, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/gzip',
-          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
-        },
-      })
-
-      if (!resp.ok) {
-        throw new Error(`Download fail: ${resp.statusText}`)
-      }
-
-      writeFileSync(target, new Uint8Array(await resp.arrayBuffer()))
-    }
-
-    console.log('Extracting...')
-    await x({
-      file: target,
-      cwd: outputDir,
+  if (existsSync(target)) {
+    rmSync(target)
+  }
+  if (!existsSync(target)) {
+    console.log(`Downloading from ${downloadUrl}`)
+    const resp = await fetch(downloadUrl, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/gzip',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+      },
     })
 
-    console.log('Updating README.md')
-
-    writeFileSync(
-      `${outputDir}/README.md`,
-      `# wa-sqlite fts5\n\nDownload from https://github.com/${REPO}\n\nTag [\`${tag}\`](${releaseUrl})\n`,
-      'utf-8',
-    )
-  } finally {
-    if (existsSync(target)) {
-      rmSync(target)
+    if (!resp.ok) {
+      throw new Error(`Download fail: ${resp.statusText}`)
     }
+
+    writeFileSync(target, new Uint8Array(await resp.arrayBuffer()))
   }
+
+  console.log('Extracting...')
+  await x({
+    file: target,
+    cwd: outputDir,
+  })
+
+  console.log('Updating README.md')
+
+  writeFileSync(
+    `${outputDir}/README.md`,
+    `# wa-sqlite fts5\n\nDownload from https://github.com/${REPO}\n\nTag [\`${tag}\`](${releaseUrl})\n`,
+    'utf-8',
+  )
 }
 
 getLatestTag()
